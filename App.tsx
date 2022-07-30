64c811c93dcf8bf856bbe5b0f97f855d5d7a20bd
Daniel Friyia <dfriyia@gmail.com>
2022-07-30 09:36:47 -0400

    Small Cleanups

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import React, {useCallback} from 'react';
-import {Button, Easing, StyleSheet, View} from 'react-native';
-
 import {
   Canvas,
   Path,
@@ -11,17 +8,17 @@ import {
   useFont,
   useValue,
 } from '@shopify/react-native-skia';
+import React from 'react';
+import {Button, Easing, StyleSheet, View} from 'react-native';
+
 import * as d3 from 'd3';
 
-const GRAPH_MARGIN = 20;
-const GRAPH_BAR_WIDTH = 8;
+interface DataPoint {
+  label: string;
+  value: number;
+}
 
-const CanvasHeight = 350;
-const CanvasWidth = 350;
-const graphHeight = CanvasHeight - 2 * GRAPH_MARGIN;
-const graphWidth = CanvasWidth - 2;
-
-const data = [
+const data: DataPoint[] = [
   {label: 'Jan', value: 50},
   {label: 'Feb', value: 100},
   {label: 'Mar', value: 350},
@@ -36,44 +33,55 @@ const data = [
   {label: 'Dec', value: 600},
 ];
 
+const GRAPH_MARGIN = 20;
+const GRAPH_BAR_WIDTH = 8;
+
+const CanvasHeight = 350;
+const CanvasWidth = 350;
+const graphHeight = CanvasHeight - 2 * GRAPH_MARGIN;
+const graphWidth = CanvasWidth - 2;
+
 const App = () => {
-  const chartCompleted = useValue(0);
-
   const font = useFont(require('./Roboto-Bold.ttf'), 10);
+  const animationState = useValue(0);
 
-  const animateChart = useCallback(() => {
-    chartCompleted.current = 0;
-
-    runTiming(chartCompleted, 1, {
-      duration: 1250,
-      easing: Easing.inOut(Easing.exp),
-    });
-  }, [chartCompleted]);
-
-  const xDomain = data.map(item => item.label);
+  const xDomain = data.map((dataPoint: DataPoint) => dataPoint.label);
   const xRange = [0, graphWidth];
   const x = d3.scalePoint().domain(xDomain).range(xRange).padding(1);
 
-  const yDomain = [0, d3.max(data, d => d.value)];
+  const yDomain = [
+    0,
+    d3.max(data, (yDataPoint: DataPoint) => yDataPoint.value),
+  ];
   const yRange = [0, graphHeight];
   const y = d3.scaleLinear().domain(yDomain).range(yRange);
 
-  const myPath = useComputedValue(() => {
-    const path = Skia.Path.Make();
+  const animate = () => {
+    animationState.current = 0;
 
-    data.map(item => {
+    runTiming(animationState, 1, {
+      duration: 1600,
+      easing: Easing.inOut(Easing.exp),
+    });
+  };
+
+  const path = useComputedValue(() => {
+    const newPath = Skia.Path.Make();
+
+    data.forEach((dataPoint: DataPoint) => {
       const rect = Skia.XYWHRect(
-        x(item.label) - GRAPH_BAR_WIDTH / 2,
+        x(dataPoint.label) - GRAPH_BAR_WIDTH / 2,
         graphHeight,
         GRAPH_BAR_WIDTH,
-        y(item.value * chartCompleted.current) * -1,
+        y(dataPoint.value * animationState.current) * -1,
       );
-      const roundedRect = Skia.RRectXY(rect, 8, 8);
-      path.addRRect(roundedRect);
+
+      const rrect = Skia.RRectXY(rect, 8, 8);
+      newPath.addRRect(rrect);
     });
 
-    return path;
-  }, [chartCompleted]);
+    return newPath;
+  }, [animationState]);
 
   if (!font) {
     return <View />;
@@ -82,23 +90,18 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Canvas style={styles.canvas}>
-        <Path path={myPath} color="purple" />
-        {data.map(item => (
+        <Path path={path} color="purple" />
+        {data.map((dataPoint: DataPoint) => (
           <Text
-            key={item.label}
+            key={dataPoint.label}
             font={font}
-            x={x(item.label) - 10}
+            x={x(dataPoint.label) - 10}
             y={CanvasHeight - 25}
-            text={item.label}
+            text={dataPoint.label}
           />
         ))}
       </Canvas>
-      <Button
-        title="Animate !"
-        onPress={() => {
-          animateChart();
-        }}
-      />
+      <Button title="Animate!" onPress={animate} />
     </View>
   );
 };
@@ -110,8 +113,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   canvas: {
-    width: CanvasWidth,
     height: CanvasHeight,
+    width: CanvasWidth,
   },
 });
 
--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+import {LogBox} from 'react-native';
+
+LogBox.ignoreAllLogs();
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
